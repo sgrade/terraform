57334195c6a4dd6dff255236bc580e0553798cde
SGrade <sgrade@users.noreply.github.com>
2020-06-06 23:24:20 +0200

    WIP: compute_instance_group

---
+++ gcp/compute_instance_group/main.tf
@@ -0,0 +1,72 @@
+provider "google" {
+
+  credentials = file(var.credentials_file)
+
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "terraform-firewall"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "22"]
+  }
+
+  target_tags = ["web"]
+}
+
+resource "google_compute_instance" "vm_instance_1" {
+  name         = "terraform-instance-1"
+  machine_type = var.machine_types[var.environment]
+  tags         = ["web"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+  }
+
+}
+
+resource "google_compute_instance" "vm_instance_2" {
+  name         = "terraform-instance-2"
+  machine_type = var.machine_types[var.environment]
+  tags         = ["web"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+  }
+
+}
+
+
+resource "google_compute_instance_group" "terraform_group" {
+  name = "terraform-group"
+  network = google_compute_network.vpc_network.self_link
+  instances = [
+    google_compute_instance.vm_instance_1.self_link,
+    google_compute_instance.vm_instance_2.self_link, 
+    ]
+}
